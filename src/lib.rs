6e616c0e3ff22347874d3d86f9285b991e7c8fba
Ammon Smith <ammon.i.smith@gmail.com>
2021-04-19 13:26:43 -0400

    Add misc/ directory.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
  * slog-mock - Mock crate for slog to compile out all logging.
  * Copyright (c) 2021 Ammon Smith
  *
- * str-macro is available free of charge under the terms of the MIT
+ * slog-mock is available free of charge under the terms of the MIT
  * License. You are free to redistribute and/or modify it under those
  * terms. It is distributed in the hopes that it will be useful, but
  * WITHOUT ANY WARRANTY. See the LICENSE file for more details.
